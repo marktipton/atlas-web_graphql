fcb682fa8167231eb23d5828bcfed8a8a4993870
mrktptn <marktipton95@gmail.com>
2024-07-01 15:05:20 -0500

    added query for id 2

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,6 +10,21 @@ const TaskType = new GraphQLObjectType({
   }
 });
 
+const tasks = [
+  {
+    id: '1',
+    title: 'Create your first webpage',
+    weight: 1,
+    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
+  },
+  {
+    id: '2',
+    title: 'Structure your webpage',
+    weight: 1,
+    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'
+  }
+];
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -17,12 +32,7 @@ const RootQuery = new GraphQLObjectType({
       type: TaskType,
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
-        return {
-          id: "1",
-          title: "areyouworking?",
-          weight: 10,
-          description: 'pleasebeworking?'
-        };
+        return tasks.find(task => task.id === args.id);
       }
     }
   }
