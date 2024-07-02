efaabd5449953a98f87480df54b19941f2092062
mrktptn <marktipton95@gmail.com>
2024-07-02 14:32:26 -0500

    added mongoose schema to project and task models

--- server/app.js
+++ server/app.js
@@ -2,6 +2,22 @@ const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 const app = express();
+const mongoose = require('mongoose');
+
+const uri = "mongodb+srv://marktipton:gr1VKmT7hhRjCb3c@cluster0.r1vmbht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to the database:', err);
+});
 
 app.use('/graphql',graphqlHTTP({
   schema: schema,
