ada8ee089e98955962491408e16ad7046de955bb
mrktptn <marktipton95@gmail.com>
2024-07-03 11:36:41 -0500

    added apollo boost to app to connect it to graphql

--- GraphQL_API/client/src/App.js
+++ GraphQL_API/client/src/App.js
@@ -6,10 +6,13 @@
 import TaskList from './components/TaskList';
 import AddTask from './components/AddTask';
 import AddProject from './components/AddProject';
-// apollo client setup
-/*
-...
-*/
+import ApolloClient from 'apollo-boost';
+
+// Apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
 function App() {
   return ( <
     div id = "main" >
