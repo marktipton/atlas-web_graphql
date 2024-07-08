d96342d8a252fdf3e9a8c183eb83af630ff7db90
mrktptn <marktipton95@gmail.com>
2024-07-08 15:52:20 -0500

    client server working again

--- GraphQL_API/client/src/App.js
+++ GraphQL_API/client/src/App.js
@@ -14,18 +14,14 @@ const client = new ApolloClient({
 });
 
 function App() {
-  return ( <
-    div id = "main" >
-    <
-    div id = "bg" > < /div> <
-    h1 > Holberton school tasks < /h1> <
-    TaskList / >
-    <
-    AddProject / >
-    <
-    AddTask / >
-    <
-    /div>
+  return (
+    <div id = "main" >
+      <div id = "bg" > </div>
+      <h1> Holberton school tasks </h1>
+    <TaskList / >
+    <AddProject / >
+    <AddTask / >
+    </div>
   );
 }
 
